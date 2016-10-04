120d478b61a1b76f451998d505e994f02f3bf687
proglan <lz376@cornell.edu>
2016-10-04 00:43:41 -0700

    json file path change

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 // particleJS
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', '/assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
